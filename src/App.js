61711ce98401361e133c193445e8a083845423fa
Tim Schönwald <timschoenwald@googlemail.com>
2022-02-02 04:40:12 +0100

    implemented design and functionality including api calls

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
+
+import favicon from "./images/favicon.png";
 
 class App extends React.Component {
-  state = {
+    state = {
+        advice: "",
+    };
 
-  }
+    componentDidMount() {
+        this.fetchAdvice();
+    }
 
-  render()  {
-    return (
-      <div className="App">
-        This is the basic setup for this React App.
-      </div>
-    );
-  }
+    fetchAdvice = () => {
+        const id = Math.floor(Math.random() * 100) - 1; //generate random id for random advice
+
+        axios
+            .get("https://api.adviceslip.com/advice/" + id.toString())
+            .then((response) => {
+                const { advice } = response.data.slip;
+
+                this.setState({ advice }); //equal to {advice: advice} since property and value have same name
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    render() {
+        const { advice } = this.state;
+
+        return (
+            <div className='app'>
+                <img className='guruImage' src={favicon} alt='guru' />
+                <div className='card'>
+                    <h1 className='heading'>{advice}</h1>
+                    <button className='button' onClick={this.fetchAdvice}>
+                        <span>I need more advice!</span>
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
